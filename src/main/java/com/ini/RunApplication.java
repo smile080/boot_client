6593dde0ff0a83f52b37c00a45ab2acee340542a
chen_jian <chenjian190000774@126.com>
2018-10-12 16:05:44 +0800

    first commit

---
+++ src/main/java/com/ini/RunApplication.java
@@ -0,0 +1,14 @@
+package com.ini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class RunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunApplication.class, args);
+	}
+}
