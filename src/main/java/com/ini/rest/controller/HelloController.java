bf411e3250381b6fe7276f9099eb445cafa6e773
chen_jian <chenjian190000774@126.com>
2018-10-18 09:19:21 +0800

    20181018

--- src/main/java/com/ini/rest/controller/HelloController.java
+++ src/main/java/com/ini/rest/controller/HelloController.java
@@ -7,19 +7,18 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class UserController {
+public class HelloController {
  
-	private final Logger logger = Logger.getLogger(UserController.class);
+	private final Logger logger = Logger.getLogger(HelloController.class);
 	
 	@Autowired
 	private DiscoveryClient client;
  
-    @RequestMapping(value = "/hello2" ,method=RequestMethod.GET)
+    @RequestMapping(value = "/hello1" ,method=RequestMethod.GET)
     public String  saveArticle() {
     	ServiceInstance instance = client.getLocalServiceInstance();
     	logger.info("/hello,host:" + instance.getHost() + ",service_id:" + instance.getServiceId());
-    	return "Hello service 2";
-    	
+    	return "Hello service 1";
     }
     
     
