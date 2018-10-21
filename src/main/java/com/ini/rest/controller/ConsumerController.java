bc2a5de50f7d1e172c5adf3e147f8420e909fb8f
chen_jian <chenjian190000774@126.com>
2018-10-21 20:27:14 +0800

    20181021 20:27

--- src/main/java/com/ini/rest/controller/ConsumerController.java
+++ src/main/java/com/ini/rest/controller/ConsumerController.java
@@ -1,6 +1,13 @@
 package com.ini.rest.controller;
 
+import java.nio.charset.Charset;
+
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.support.BasicAuthorizationInterceptor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +20,18 @@ public class ConsumerController {
     
     @RequestMapping(value="/ribbon-consumer",method=RequestMethod.GET)
     public String helloConsumer() {
-        return restTemplate.getForEntity("http://client1114:1114//hello3", String.class).getBody();
+    	String username="admin";
+    	String password="123456";
+    	String url="http://client1114:1114//hello3";
+    	
+    	restTemplate.getInterceptors().add(new BasicAuthorizationInterceptor(username, password));
+        return restTemplate.getForEntity(url, String.class).getBody();
+        
+       /* String auth_Str = username + ":" + password;
+        byte[] encodedAuth = Base64.encodeBase64(auth_Str.getBytes(Charset.forName("US-ASCII")));
+        String authHeader = "Basic " + new String(encodedAuth);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("authorization", authHeader);
+        return restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(null, headers),String.class).getBody();*/
     }
 }
