b499a9a8d14ba8339d835e2f4e6bb989372a3c99
chen_jian <chenjian190000774@126.com>
2018-10-24 11:08:06 +0800

    20181024 11:07

--- src/main/java/com/ini/RunApplicationClient.java
+++ src/main/java/com/ini/RunApplicationClient.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
